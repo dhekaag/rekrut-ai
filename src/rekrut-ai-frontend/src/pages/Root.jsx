8abb154ef87dd6db3d62e3019749ae2b2368a56f
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-02-26 23:27:20 +0700

    fix: fix padding inconditional

--- src/rekrut-ai-frontend/src/pages/Root.jsx
+++ src/rekrut-ai-frontend/src/pages/Root.jsx
@@ -5,7 +5,7 @@ const Root = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow pt-10">
+      <main className="flex-grow">
         <Outlet />
       </main>
     </div>
