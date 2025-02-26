3bf84c641468c2d0a3edb33f5182d43c2aaa187b
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-02-26 23:13:06 +0700

    add card conflicts

--- src/rekrut-ai-frontend/src/components/Fragments/InterviewCard.jsx
+++ src/rekrut-ai-frontend/src/components/Fragments/InterviewCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/Card";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
