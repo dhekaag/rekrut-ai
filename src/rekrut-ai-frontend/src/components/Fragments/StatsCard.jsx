bc2fc630a7ecb551c0f578bf162d24548557fe7f
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-02-26 23:10:00 +0700

    fix: fix named card

--- src/rekrut-ai-frontend/src/components/Fragments/StatsCard.jsx
+++ src/rekrut-ai-frontend/src/components/Fragments/StatsCard.jsx
@@ -4,7 +4,7 @@ export default function StatsCard({ bgcolor, color, border }) {
       className={`border ${border} ${bgcolor} ${color} w-40 h-32 p-3 rounded-lg text-center flex flex-col items-center justify-center transition-all hover:shadow-md`}
     >
       <h1 className="text-2xl font-bold">90%</h1>
-      <p className="text-xs font-medium mt-1">
+      <p className="text-xs font-semibold mt-1">
         Merasa terbantu dengan latihan terlebih dahulu bersama RekrutAI
       </p>
     </div>
